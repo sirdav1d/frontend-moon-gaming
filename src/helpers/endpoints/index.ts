edc0dafd520d3559f7b17ea04c167de7cb95c85e
sirdav1d <ddavid.diniz@gmail.com>
2022-10-26 21:24:25 -0300

    change: endpoint

--- src/helpers/endpoints/index.ts
+++ src/helpers/endpoints/index.ts
@@ -1,14 +1,14 @@
 import { auth } from './auth';
-import {profile } from './profile';
+import { profile } from './profile';
 import { game } from './game';
 import { gender } from './gender';
 import { user } from './user';
 
 export const endpoint = {
-    baseUrl: "estjs-moon-gaming-api-production.up.railway.app",
-    ...auth,
-    ...user,
-    ...gender,
-    ...profile,
-    ...game,
+	baseUrl: 'https://api-moon-g.onrender.com',
+	...auth,
+	...user,
+	...gender,
+	...profile,
+	...game,
 };
